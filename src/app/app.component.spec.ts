2092d00b06ee1c8d88d97cc89f6b33565fd356a9
BrancuAlexandru <alexandrubrancu@yahoo.com>
2023-05-15 23:35:03 +0300

    Renamed the project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SmallChessApp'`, () => {
+  it(`should have as title 'AngularLearnerApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SmallChessApp');
+    expect(app.title).toEqual('AngularLearnerApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SmallChessApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularLearnerApp app is running!');
   });
 });
